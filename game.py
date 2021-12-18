976f0653b0eaa68faa11ca6e2289993703c2b9af
Alpha Cian <st10083010@gmail.com>
2021-12-18 16:27:45 +0800

    update: game.py

--- game.py
+++ game.py
@@ -3,14 +3,36 @@
 import pygame
 
 FPS = 60
+TITLE = pygame.display.set_caption("game")  # 標題
+WIDTH = 800
+HEIGHT = 600
+
 backgroundColor = (200, 100, 50)
-width = 800
-height = 600
 
 pygame.init()  # 遊戲初始化
 
-screen = pygame.display.set_mode((width, height))  # 解析度(視窗大小 tuple)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # 解析度(視窗大小 tuple)
 clock = pygame.time.Clock()  # 對時間進行管理與操控
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)  # 引入預設函式
+        self.image = pygame.Surface((50, 40))  # 玩家面積
+        self.image.fill((0, 255, 0))  # 玩家顏色
+        self.rect = self.image.get_rect()  # 定位
+        self.rect.center = (WIDTH/2, HEIGHT/2)  # 初始座標 左上角為(0, 0) 正中央寫法
+
+    def update(self):
+        self.rect.y += 3
+        if self.rect.top > HEIGHT:
+            self.rect.bottom = 0
+
+
+allSprites = pygame.sprite.Group()  # 建立群組，群組內的物件通通會被一起控制
+player = Player()
+allSprites.add(player)
+
 
 running = True
 
@@ -22,8 +44,10 @@ while running:
             running = False
 
     # 更新遊戲
+    allSprites.update()  # 執行allSprites中每個物件的update函式
     # 畫面顯示
     screen.fill(backgroundColor)  # RGB(tuple)
+    allSprites.draw(screen)  # 把玩家畫到螢幕上
     pygame.display.update()
 
 
